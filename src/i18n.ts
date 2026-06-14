2449273a1fa9cb0ca053f6f5466ffc9aea0c64b9
fr1en16 <pilipuyk.21@gmail.com>
2026-06-15 00:43:51 +0500

    fix: remove conflicting old src/i18n.ts that shadowed i18n/ directory

--- src/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async ({ locale }) => {
-  const resolvedLocale = locale ?? 'ru';
-  return {
-    locale: resolvedLocale,
-    messages: (await import(`../messages/${resolvedLocale}.json`)).default,
-  };
-});
